be9dd9f272ccb21fd1be03c3861ee40a7587bb82
Sudip Sarker <sudip@itconquest.com>
2017-10-11 14:43:45 +0600

    update

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\History;
 use App\Models\Location;
+
 use Illuminate\Http\Request;
 use App\Http\Requests;
 
@@ -11,7 +13,7 @@ class LocationController extends Controller
 {
     public function getLocation(Request $request)
     {
-        $location = Location::where('user_id', $request->user()->id)->latest()->first();
+        $location = Location::where('user_id', $request->user()->id)->get();
         //dd($request->user()->id);
         if ($location) {
             return response()->json($location);
@@ -27,17 +29,22 @@ class LocationController extends Controller
     public function storeLocation(Request $request)
     {
 
-        $request->request->add(['user_id' => $request->user()->id]);
-        //dd($request->all());
-        if (Location::create($request->all())) {
+        $user_id = $request->user()->id;
+        $request->request->add(['user_id' => $user_id]);
+        History::create($request->all());
+        $location = Location::where('user_id', $user_id)->first();
+        if ($location){
+            $location->latitude = $request->get("latitude");
+            $location->longitude = $request->get("longitude");
+            $location->save();
+            return response()->json([
+                'msg' => 'Location Update successfully'
+            ], 201);
+        }else{
+            Location::create($request->all());
             return response()->json([
                 'msg' => 'Location Create successfully'
             ], 201);
-        } else {
-            return response()->json([
-                'msg' => 'error',
-                'error' => 'Location cannot create'
-            ], 400);
         }
 
     }
--- app/Models/History.php
+++ app/Models/History.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Location extends Model
+class History extends Model
 {
     protected $fillable = [
         'user_id','latitude', 'longitude',
